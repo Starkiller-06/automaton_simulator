5d9e3637c46b99aff380f97fe46db375784044af
Ximena <ximenaruber0698@gmail.com>
2025-12-02 22:48:59 -0600

    Home page design imprved. Modificaions in App css and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css'
 import { useNavigate } from 'react-router-dom';
+import PixelBlast from './components/PixelBlast';
 
 export default function App() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Automaton Simulator</h1>
-      <button type="button" onClick={() => navigate('/simulator')}>Simulator</button>
+    <div style={{ width:'100%', height: '100vh', position: 'fixed', inset: 0, overflow: 'hidden' }}>
+      <PixelBlast
+        variant="square"
+        pixelSize={6}
+        color="#B19EEF"
+        patternScale={3}
+        patternDensity={1.2}
+        pixelSizeJitter={0.5}
+        enableRipples
+        rippleSpeed={0.4}
+        rippleThickness={0.12}
+        rippleIntensityScale={1.5}
+        liquidStrength={0.12}
+        liquidRadius={1.2}
+        liquidWobbleSpeed={5}
+        speed={0.6}
+        edgeFade={0.25}
+        transparent
+      />
+           
+      <div className='content-container'>
+        <h1 className='app-header'>Automaton Simulator</h1>   
+        <button className='simulator-btn' type="button" onClick={() => navigate('/simulator')}>Try it out!</button>
+      </div>
+      
     </div>
   )
 }
